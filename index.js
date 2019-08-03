5ade9a2cffb55cd81c260cb08021b2305deecc0a
Cesare Ferrari <cesare@maryandferrari.com>
2019-08-03 12:35:52 -0400

    Create index.js

--- index.js
+++ index.js
@@ -12,3 +12,12 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+require('dotenv').config();
+
+const server = require('./server.js');
+const port = process.env.PORT ? process.env.PORT : 5000;
+
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
